83b261bf5ad556999960269cc5e0d26ad7cf517f
gauravsh0812 <gauravs@arizona.edu>
2023-10-14 23:32:12 -0700

    updating

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -13,12 +13,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "--images_folder_name",
     help="images folder name in data folder",
-    default="images_processed",
+    default="oimages",
 )
 parser.add_argument(
     "--equations_file_name",
     help="equations file name in the data folder",
-    default="im2latex.lst",
+    default="olatex.lst",
 )
 parser.add_argument(
     "--data_path",
